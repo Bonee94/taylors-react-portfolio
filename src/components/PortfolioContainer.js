65f386b47a79f2a6602419dc1a9907322520f8d0
Bonee94 <bonee94@yahoo.com>
2023-03-15 20:20:58 -0500

    Created container to house app and page rendering

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import About from './pages/About';
+
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // this is checking the value of current page state and returning the proper page
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
