a6497d3f4e9df69cd6d80a3931e86e117fd0958d
Bonee94 <bonee94@yahoo.com>
2023-03-15 22:56:39 -0500

    Adjusted returned JSX background image to cover behind entire div

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,13 @@
 import React from "react";
 import NavLinks from "./NavLinks";
+import '../styles/Header.css'
 
 function Header ({ currentPage, handlePageChange }) {
     return (
-    <header>
+    <header> 
         <div className="hdr-section-1">
             <h1 className="my-name-hdr">Taylor Turner</h1>
             <NavLinks currentPage={currentPage} handlePageChange={handlePageChange}/>
-        </div>
-        <div>
-            <img className="hdr-banner-img" src="./assets/images/Screenshot 2022-10-21 223902.png" alt=""/>
-            <img className="profile-picture" src="./assets/images/Taylor-profile-pic.png" alt=""/>
-        </div>
-        <div className="subtitle-container">
-            <h2 className="subtitle">Cool subtitle here!</h2>
         </div>
     </header>
     );
