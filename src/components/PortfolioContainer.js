b2f2d09fca7541d515b221366ab3461201a58ee2
Bonee94 <bonee94@yahoo.com>
2023-03-16 17:59:28 -0500

    Added portfolio page rendering to the app container

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 import '../styles/PortfolioContainer.css'
 
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage, setCurrentPage] = useState('Portfolio');
 
   // this is checking the value of current page state and returning the proper page
   const renderPage = () => {
     if (currentPage === 'About') {
       return <About />;
+    };
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
     }
   };
 
