3c1ce585da71f942b3e830889ffb6147c22ebfa0
Bonee94 <bonee94@yahoo.com>
2023-03-16 18:02:01 -0500

    Created projects array to create projects object dynamically to be called by projects page

---
+++ src/assets/projects/projectsArr.js
@@ -0,0 +1,47 @@
+export default function projectsArr() {
+    const projectsArr = [
+    {
+        id: 1,
+        href: "https://spangled-banner-realty.herokuapp.com/",
+        imgSrc: "sbRealty",
+        imgAlt: "SB Realty Project",
+        tileSize: "Big",
+        projTitle: "SB Realty Group",
+        githubRepo: "https://github.com/LuisChevere/ProjectBCL"
+    },
+    {
+        href: "https://rdalby2002.github.io/national-park-locator/",
+        imgSrc: "parkLocator",
+        imgAlt: "National Park Locator Project",
+        tileSize: "Small",
+        projTitle: "National Park Route Planner",
+        githubRepo: "https://github.com/rdalby2002/national-park-locator"
+    },
+    {
+        href: "https://bonee94.github.io/Module-1-Challenge/",
+        imgSrc: "horiseon",
+        imgAlt: "Horiseon Application",
+        tileSize: "Small",
+        projTitle: "Horiseon Webpage",
+        githubRepo: "https://github.com/Bonee94/Module-1-Challenge"
+    },
+    {
+        href: "#",
+        imgSrc: "fistBump",
+        imgAlt: "",
+        tileSize: "Small",
+        projTitle: "",
+        githubRepo: ""
+    },
+    {
+        href: "#",
+        imgSrc: "tree",
+        imgAlt: "",
+        tileSize: "Small",
+        projTitle: "",
+        githubRepo: ""
+    },
+];
+
+return projectsArr
+}
