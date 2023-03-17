cd30f6ed3b29fe9011553a5adbf87b3c442c350b
Bonee94 <bonee94@yahoo.com>
2023-03-17 02:19:33 -0500

    Added more projects to array for page rendering

--- src/assets/projects/projectsArr.js
+++ src/assets/projects/projectsArr.js
@@ -1,21 +1,44 @@
 export default function projectsArr() {
     const projectsArr = [
     {
-        id: 1,
+        href: "https://i-am-j-a-t-e.herokuapp.com/",
+        imgSrc: "JATE",
+        imgAlt: "just another text editor",
+        tileSize: "Big",
+        projTitle: "J.A.T.E (Just Another Text Editor",
+        githubRepo: "https://github.com/Bonee94/Taylors-Text-Editor"
+    },
+    {
         href: "https://spangled-banner-realty.herokuapp.com/",
         imgSrc: "sbRealty",
         imgAlt: "SB Realty Project",
-        tileSize: "Big",
+        tileSize: "Small",
         projTitle: "SB Realty Group",
         githubRepo: "https://github.com/LuisChevere/ProjectBCL"
     },
     {
         href: "https://rdalby2002.github.io/national-park-locator/",
         imgSrc: "parkLocator",
-        imgAlt: "National Park Locator Project",
+        imgAlt: "National Park Locator Project image",
         tileSize: "Small",
         projTitle: "National Park Route Planner",
         githubRepo: "https://github.com/rdalby2002/national-park-locator"
+    },
+    {
+        href: "https://hidden-fjord-74981.herokuapp.com/",
+        imgSrc: "techBlog",
+        imgAlt: "tech blog project image",
+        tileSize: "Big",
+        projTitle: "My Tech Blog",
+        githubRepo: "https://github.com/Bonee94/Taylors-Tech-Blog"
+    },
+    {
+        href: "https://bonee94.github.io/Taylors-Weather-Application/",
+        imgSrc: "weatherApp",
+        imgAlt: "weather application project image",
+        tileSize: "Small",
+        projTitle: "Weather Application",
+        githubRepo: "https://github.com/Bonee94/Taylors-Weather-Application"
     },
     {
         href: "https://bonee94.github.io/Module-1-Challenge/",
@@ -25,22 +48,7 @@ export default function projectsArr() {
         projTitle: "Horiseon Webpage",
         githubRepo: "https://github.com/Bonee94/Module-1-Challenge"
     },
-    {
-        href: "#",
-        imgSrc: "fistBump",
-        imgAlt: "",
-        tileSize: "Small",
-        projTitle: "",
-        githubRepo: ""
-    },
-    {
-        href: "#",
-        imgSrc: "tree",
-        imgAlt: "",
-        tileSize: "Small",
-        projTitle: "",
-        githubRepo: ""
-    },
+    
 ];
 
 return projectsArr
