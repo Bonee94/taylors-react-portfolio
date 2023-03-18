39c7d55f8a08603b19b89eff1c567099855fb8f7
Bonee94 <bonee94@yahoo.com>
2023-03-18 17:23:54 -0500

    Corrected fetch url to fetch resume pdf and store as a blob for download

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,7 +5,9 @@ export default function Resume() {
     <div className='page-container'>
       <div className='page-card'>
         <button onClick={() => {
-            fetch("/src/assets/Resume.pdf")
+            const site = window.location.href;
+            const url = site.split('\#')[0]+'resume.pdf'
+            fetch(url)
             .then((response) => {
                 response.blob().then(blob => {
                     const fileURL = window.URL.createObjectURL(blob);
@@ -14,7 +16,8 @@ export default function Resume() {
                     alink.download = 'SamplePDF.pdf';
                     alink.click();
                 })}) 
-        }}>Download Resume</button>
+        
+            }}>Download Resume</button>
       </div>
     </div>
   );
