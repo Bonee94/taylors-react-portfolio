026439c3544ff80ec201108cdd274debfa97e978
Bonee94 <bonee94@yahoo.com>
2023-03-19 03:03:01 -0500

    Changed the jsx rendering for each project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,37 +8,22 @@ export default function Projects({
   imgSrc,
   imgAlt,
   projTitle,
-  tileSize,
 }) {
-  if (tileSize === "Small") {
-    return (
-      <div className="small-work-tiles tiles">
-        <a href={href}>
-          <Images imgSrc={imgSrc} imgAlt={imgAlt} />
-        </a>
-        <div className="project-titles">
-            <h2><a href={href} className="project-title-link">{projTitle}</a></h2>
-            <h3>
-            <a href={githubRepo}>Github Repo</a>
-            </h3>
-        </div>
+  return (
+    <div className="work-tiles tiles">
+      <a href={href}>
+        <Images imgSrc={imgSrc} imgAlt={imgAlt} />
+      </a>
+      <div className="project-titles">
+        <h2>
+          <a href={href} className="project-title-link">
+            {projTitle}
+          </a>
+        </h2>
+        <h3>
+          <a href={githubRepo}>Github Repo</a>
+        </h3>
       </div>
-    );
-  }
-
-  if (tileSize === "Big") {
-    return (
-      <div className="big-work-tiles tiles">
-        <a href={href}>
-          <Images imgSrc={imgSrc} imgAlt={imgAlt} />
-        </a>
-        <div className="project-titles">
-            <h1><a href={href} className="project-title-link">{projTitle}</a></h1>
-            <h3>
-            <a href={githubRepo}>Github Repo</a>
-            </h3>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
