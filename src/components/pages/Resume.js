d66f8b5960d4210705932f936d494ad7c3950ceb
Bonee94 <bonee94@yahoo.com>
2023-03-19 03:04:03 -0500

    Added resume proficiencies ul to page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../../styles/Resume.css';
 
 export default function Resume() {
   return (
     <div className='page-container'>
       <div className='page-card'>
-        <button onClick={() => {
+        <h1>Resume</h1>
+        <h2 id='resume-download-hdr'>Click to download my <button id="resume-submit" onClick={() => {
             const site = window.location.href;
             const url = site.split('#')[0]+'/resume.pdf';
             fetch(url)
@@ -17,7 +19,35 @@ export default function Resume() {
                     alink.click();
                 })}) 
         
-            }}>Download Resume</button>
+            }}>Resume</button></h2>
+        
+
+            <div className='prof-box'>
+              <div className='prof-section'>
+                <h2>Front-end Proficiencies</h2>
+                <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Javascript</li>
+                  <li>JQuery</li>
+                  <li>Responsive Design</li>
+                  <li>React</li>
+                  <li>Bootstrap</li>
+                </ul>
+              </div>
+              <div className='prof-section'>
+                <h2>Back-end Proficiencies</h2>
+                <ul>
+                  <li>APIs</li>
+                  <li>Node</li>
+                  <li>Express</li>
+                  <li>MySQL, Sequelize</li>
+                  <li>MongoDB, Mongoose</li>
+                  <li>REST</li>
+                  <li>GraphQL</li>
+                </ul>
+              </div>
+            </div>
       </div>
     </div>
   );
