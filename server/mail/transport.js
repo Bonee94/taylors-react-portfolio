f808b34bb3f9d52d30111ff9cb186ac90fbb842e
Bonee94 <bonee94@yahoo.com>
2023-04-06 21:46:10 -0500

    Changed SMTP port

--- server/mail/transport.js
+++ server/mail/transport.js
@@ -6,8 +6,8 @@ module.exports = async function getTransport() {
 
   let transport = nodemailer.createTransport({
     host: host,
-    port: 587,
-    secure: process.env.NODE_ENV === "production",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER || "dessie.nolan@ethereal.email",
       pass: process.env.EMAIL_PASS || "U4S97shYZT1E5ByJUD",
