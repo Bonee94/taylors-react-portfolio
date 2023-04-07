df3a10220fecefd1b8ec6a92116e98588d3238f3
Bonee94 <bonee94@yahoo.com>
2023-04-06 20:03:43 -0500

    Created resolver for back end functionality for sending myself an email when a contact message is submitted

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,39 @@
+const nodemailer = require("nodemailer")
+const getTransport = require("../mail/transport")
+
+const resolvers = {
+    Query: {
+      console: async () => {
+        console.log("Hello")
+        return
+      }
+    },
+
+    Mutation: {
+      sendMessage: async (parent, { emailInput, messageInput, fromInput }) => {
+        console.log(`Email: ${emailInput}, From: ${fromInput}, Message: ${messageInput}`)
+        try {
+          const transport = await getTransport();
+          transport.sendMail({
+            from: "Portfolio <TaylorTurner@portfolio.com>",
+            to: "taylor.portfolio.email@gmail.com",
+            subject: `Portfolio Test`,
+            html: `<h1>From: ${fromInput}</h1>
+            <h1>Email: ${emailInput}</h1>
+             <h1>Message:</h1>
+            <p>${messageInput}</p>
+            `
+          }).then(info => {
+            console.log("Message Id:", info.messageId); 
+            console.log("Url:", nodemailer.getTestMessageUrl(info)); 
+          })
+        } catch (err) {
+          console.log(err);
+        }
+
+        return { message: "Thank for contacting me! I will get back to you soon.", error: false };
+      }
+    }
+}
+
+module.exports = resolvers;
