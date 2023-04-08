df5f4f5c8ead1f379fb057bf056034f8fd156819
Taylor Turner <bonee94@yahoo.com>
2023-04-08 17:17:00 -0500

    Updates

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
-
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  config.plugins.push(new NodePolyfillPlugin({
-    excludeAliases: ['console']
-  }))
-  return config
-}
