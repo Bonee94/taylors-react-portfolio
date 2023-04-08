c9d5577a17248d8aeb224520d9243dcabe675053
Bonee94 <bonee94@yahoo.com>
2023-04-07 20:27:42 -0500

    Adjusted transport Auth object... Again.

--- server/mail/transport.js
+++ server/mail/transport.js
@@ -1,19 +1,26 @@
-const nodemailer = require('nodemailer');
-//const {user, pass} =
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+
+console.log(process.env)
+
+
+//const {user, pass} = 
 const host = "smtp.gmail.com";
 
 module.exports = async function getTransport() {
-
   let transport = nodemailer.createTransport({
     host: host,
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
-      user: process.env.EMAIL_USER || "dessie.nolan@ethereal.email",
-      pass: process.env.EMAIL_PASS || "U4S97shYZT1E5ByJUD",
+      type: "OAuth2",
+      user: process.env.REACT_APP_EMAIL_USER,
+      clientId: process.env.REACT_APP_CLIENT_ID,
+      clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+      refreshToken: process.env.REACT_APP_REFRESH_TOKEN,
+      accessToken: process.env.REACT_APP_ACCESS_TOKEN,
     },
   });
 
-  return transport
+  return transport;
 };
-
