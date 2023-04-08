ae34043aabc4c4f6668c4d7beeb09796ee66f856
Bonee94 <bonee94@yahoo.com>
2023-04-08 01:50:33 -0500

    Added alias exclusion for node poly fill plugin

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,6 +2,8 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 module.exports = function override(config, env) {
   // do stuff with the webpack config...
-  config.plugins.push(new NodePolyfillPlugin())
+  config.plugins.push(new NodePolyfillPlugin({
+    excludeAliases: ['console']
+  }))
   return config
 }
