daef3ba9c3bd6d032136a41eee128b0f4384031d
Taylor Turner <bonee94@yahoo.com>
2023-04-09 01:18:00 -0500

    Created Modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "../styles/Modal.css";
+
+export default function Modal() {
+
+  return (
+    <div className="container">
+        <div className="modal-box">
+            <div className="modal">
+                <h1 className="shimmer-anim-1">
+                    <span>T</span>
+                    <span>h</span>
+                    <span>a</span>
+                    <span>n</span>
+                    <span>k</span>
+                    <span> </span>
+                    <span>y</span>
+                    <span>o</span>
+                    <span>u</span>
+                    <span> </span>
+                    <span>f</span>
+                    <span>o</span>
+                    <span>r</span>
+                    <span> </span>
+                    <span>c</span>
+                    <span>o</span>
+                    <span>n</span>
+                    <span>t</span>
+                    <span>a</span>
+                    <span>c</span>
+                    <span>t</span>
+                    <span>i</span>
+                    <span>n</span>
+                    <span>g</span>
+                    <span> </span>
+                    <span>m</span>
+                    <span>e</span>
+                    <span>!</span>
+                </h1>
+                <p><h2 className="shimmer-anim-2">
+                    <span>I</span>
+                    <span> </span>
+                    <span>w</span>
+                    <span>i</span>
+                    <span>l</span>
+                    <span>l</span>
+                    <span> </span>  
+                    <span>g</span>
+                    <span>e</span>
+                    <span>t</span>
+                    <span> </span>
+                    <span>b</span>
+                    <span>a</span>
+                    <span>c</span>
+                    <span>k</span>
+                    <span> </span>
+                    <span>t</span>
+                    <span>o</span>
+                    <span> </span>
+                    <span>y</span>
+                    <span>o</span>
+                    <span>u</span>
+                    <span> </span>
+                    <span>s</span>
+                    <span>h</span>
+                    <span>o</span>
+                    <span>r</span>
+                    <span>t</span>
+                    <span>l</span>
+                    <span>y</span>
+                    <span>.</span>
+                    <span>.</span>
+                    <span>.</span>
+                </h2></p>
+            </div>  
+        </div>  
+    </div>
+  );
+}
