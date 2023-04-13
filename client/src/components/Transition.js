7ea793f10df5d8c28e1a0295e631e34fb2d56e7b
Taylor Turner <bonee94@yahoo.com>
2023-04-12 19:03:15 -0500

    Added a timer to remove the transition after it completes so that buttons are clickable

--- client/src/components/Transition.js
+++ client/src/components/Transition.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Transition.scss";
 
 export default function Transition({transition}) {
 
+  const [hidden, setHidden] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHidden(true)
+    }, 1000);
+  })
+
   return (
-        <div className={transition ? "overlay show" : "overlay"}>
+        <div className={
+          transition ? "overlay show" : 
+          hidden ? "overlay hidden" : "overlay" }>
             <div className="bar"></div>
             <div className="bar"></div>
             <div className="bar"></div>
