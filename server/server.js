2f417f0397b5f67af420413006231f2c9ae7e2e8
Taylor Turner <bonee94@yahoo.com>
2023-04-12 19:19:27 -0500

    Adjusted static assets for heroku deployment

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/taylors-react-portfolio", express.static(path.join(__dirname, "../client/build")));
+app.use("/", express.static(path.join(__dirname, "../client/build")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
