109ea126dbc929e86fc13b6435d810d39cb9543b
Taylor Turner <bonee94@yahoo.com>
2023-04-27 01:50:39 -0500

    Added light dark switcher and split animations

--- client/src/components/LightDarkCandles.js
+++ client/src/components/LightDarkCandles.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/lightDarkCandles.css";
 
-export default function LightDarkCandles() {
+export default function LightDarkCandles({}) {
+    const [animLight, setAnimLight] = useState(false);
+
+    const handleColorMode = () => {
+        setAnimLight(!animLight)
+    }
+
+
   return (
-      <div className="wrapper">
+      <button onClick={handleColorMode}>
+        <div className="wrapper">
+          <span id="mode">{animLight ? "Light" : "Dark"}</span>
         <div className="candles">
-          <div className="light__wave"></div>
-          <div className="candle1">
+          <div className={`light__wave ${animLight ? "light__waveAnim": ""}`}></div>
+          <div className={`candle1 ${animLight ? "": "candle1Anim"}`}>
             <div className="candle1__body">
               <div className="candle1__eyes">
                 <span className="candle1__eyes-one"></span>
                 <span className="candle1__eyes-two"></span>
               </div>
-              <div className="candle1__mouth"></div>
+              <div className={`${animLight ? "": "candle1_mouthAnim"}`}></div>
             </div>
             <div className="candle1__stick"></div>
           </div>
 
-          <div className="candle2">
+          <div className={`candle2 ${animLight ? "candle2AnimOn": "candle2Anim"}`}>
             <div className="candle2__body">
               <div className="candle2__eyes">
-                <div className="candle2__eyes-one"></div>
-                <div className="candle2__eyes-two"></div>
+                <div className={`candle2__eyes-one ${animLight ? "candle2__eyes-oneAnimOn": "candle2__eyes-oneAnim"}`}></div>
+                <div className={`candle2__eyes-two ${animLight ? "candle2__eyes-twoAnimOn": "candle2__eyes-twoAnim"}`}></div>
               </div>
             </div>
             <div className="candle2__stick"></div>
           </div>
-          <div className="candle2__fire"></div>
+          <div className={`candle2__fire ${animLight ? "candle2__fireAnim": "candle2__fireAnimOut"}`}></div>
           <div className="sparkles-one"></div>
           <div className="sparkles-two"></div>
-          <div className="candle__smoke-one"></div>
+          <div className={`candle__smoke-one ${animLight ? "": "candle__smoke-oneAnim"}`}></div>
           <div className="candle__smoke-two"></div>
         </div>
       </div>
+      </button>
   );
 }
