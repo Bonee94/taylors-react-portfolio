3e7407f32eb02a5b8542f8bcafec08f7ca7ab702
Taylor Turner <bonee94@yahoo.com>
2023-06-05 21:50:33 -0500

    Added memory usage console.log

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 require("dotenv").config();
+const process = require('process')
 
 const { typeDefs, resolvers } = require("./schemas");
 
@@ -26,6 +27,12 @@ app.get("/", (req, res) => {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // An example displaying the respective memory
+  // usages in megabytes(MB)
+  for (const [key,value] of Object.entries(process.memoryUsage())){
+    console.log(`Memory usage by ${key}, ${value/1000000}MB `)
+  }
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
