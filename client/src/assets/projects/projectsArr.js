34a6ab1c22545892b24b57ebfafbb4e624888f64
Taylor Turner <bonee94@yahoo.com>
2023-08-01 01:03:23 -0500

    Updated project information with new project

--- client/src/assets/projects/projectsArr.js
+++ client/src/assets/projects/projectsArr.js
@@ -2,6 +2,14 @@ export default function projectsArr() {
     const projectsArr = [
     {
         id: 1,
+        href: "https://bigginsdoorrefinishing.com",
+        imgSrc: "bdrApp",
+        imgAlt: "Biggin's Door Refinishing Logo",
+        projTitle: "Biggin's Door Refinishing",
+        info: "Introducing Biggin's Door Refinishing Web Application—a sophisticated and comprehensive MERN stack solution, deployed on a secured AWS EC2 instance, that caters to the needs of a local small business in San Antonio. Seamlessly integrating both a public-facing interface and an efficient administration system, this application serves as a one-stop hub for customers and business personnel alike. <br><br>On the public side, customers are greeted with an intuitive and informative platform that effortlessly provides essential information about the business's services, contact details, and testimonials from satisfied clients. The user-friendly design ensures a smooth browsing experience, enhancing engagement and customer satisfaction. <br><br>Behind the scenes, the administration side empowers the business owners with complete control over the website's content, enabling them to effortlessly update pages, manage user accounts, and utilize a streamlined inventory system. Furthermore, the application incorporates robust email functionality to facilitate seamless communication with customers, ensuring a professional and responsive approach."
+    },
+    {
+        id: 2,
         href: "https://rappor-social-media.herokuapp.com/",
         imgSrc: "Rappor",
         imgAlt: "Rappor Social Media Logo",
@@ -10,7 +18,7 @@ export default function projectsArr() {
         info: "Rappor is a social media application that prioritizes connection and engagement through commenting and downvoting. Built using the popular MERN stack, Rappor is designed to foster interaction and conversation among users. With its focus on user feedback and interaction, Rappor aims to provide a unique and engaging social media experience that sets it apart from other platforms. While still under development, Rappor holds great promise for those seeking a more interactive and meaningful social media experience."
     },
     {
-        id: 2,
+        id: 3,
         href: "https://i-am-j-a-t-e.herokuapp.com/",
         imgSrc: "JATE",
         imgAlt: "just another text editor",
@@ -37,7 +45,7 @@ export default function projectsArr() {
         info: "National Park Locator is a web application designed for nature lovers and adventure seekers who want to explore the beauty of the US national parks. The application uses a search input to filter national parks by state and then retrieves information from an API to display relevant details about each park such as its location, activities, and amenities. The user-friendly interface allows for quick and easy navigation, making it simple to find the perfect park for your next adventure. National Park Locator's use of standard Javascript, CSS and HTML ensures a seamless user experience across multiple platforms and devices. With its intuitive design and wealth of information, National Park Locator is the perfect tool for planning your next outdoor excursion."
     },
     {
-        id: 3,
+        id: 6,
         href: "https://hidden-fjord-74981.herokuapp.com/",
         imgSrc: "techBlog",
         imgAlt: "tech blog project image",
@@ -46,7 +54,7 @@ export default function projectsArr() {
         info: "My Tech Blog is a web application that utilizes the Model-View-Controller (MVC) design pattern to create a platform for sharing and discussing the latest trends and news in the tech industry. The application features a user-friendly interface that allows for easy navigation and engagement with other tech enthusiasts. Users can create accounts, post articles, and interact with others through comments. My Tech Blog's use of a SQL database through MySql and Sequelize ensures efficient and reliable data storage. With its intuitive design and cutting-edge technology, My Tech Blog provides a top-notch user experience for tech enthusiasts and bloggers alike."
     },
     {
-        id: 6,
+        id: 7,
         href: "https://bonee94.github.io/Taylors-Weather-Application/",
         imgSrc: "weatherApp",
         imgAlt: "weather application project image",
@@ -55,7 +63,7 @@ export default function projectsArr() {
         info: "This weather application is a simple yet powerful tool for anyone who wants to check the latest weather conditions for any city or state. The application uses standard HTML, CSS and JavaScript to provide a user-friendly interface that allows users to input their desired location and retrieve real-time weather information. The application displays detailed information about the current temperature, humidity, wind speed, and precipitation levels, and provides a 5-day forecast to help users plan their activities in advance. With its reliable data sources and easy-to-use interface, this weather application is the perfect tool for anyone who wants to stay informed about the weather in their area."
     },
     {
-        id: 7,
+        id: 8,
         href: "https://bonee94.github.io/Module-1-Challenge/",
         imgSrc: "horiseon",
         imgAlt: "Horiseon Application",
